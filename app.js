64596b600a6257ff9056cca9effea0ac88359317
yinglin93 <yrlin0015@gmail.com>
2023-12-10 22:49:18 +0800

    build: add static files for style and add listing page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.render('index')
+  res.render('index', {restaurants: restaurants})
 })
 
 app.get('/restaurant/:id', (req, res) => {
