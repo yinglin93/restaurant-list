7d1508a8c4c667d9afb432b5317f0ad23defbe02
yinglin93 <yrlin0015@gmail.com>
2023-12-10 22:24:50 +0800

    build: use express-handlebars ad view engine

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('listing restaurants')
+  res.render('index')
 })
 
 app.get('/restaurant/:id', (req, res) => {
